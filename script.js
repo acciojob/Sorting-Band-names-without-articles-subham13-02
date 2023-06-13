197994b1a783ab739703f98bfb00534adc8fd92e
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-06-13 16:36:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-() => {
-  // cy.get("li").should("have.length.at.least", 1);
-  let tags_content = [];
-  const li_tags_count = document.getElementsByTagName('li').length;
+// () => {
+//   cy.get("li").should("have.length.at.least", 1);
+//   let tags_content = [];
+//   const li_tags_count = document.getElementsByTagName('li').length;
 
-  // Getting li tags content and checking if they are without any articles and in sorted order
-  for (let index = 0; index < li_tags_count; index++) {
-    cy.get("li").eq(index).then($el => {
-      tags_content.push($el.text());
-    });
-  }
+//   // Getting li tags content and checking if they are without any articles and in sorted order
+//   for (let index = 0; index < li_tags_count; index++) {
+//     cy.get("li").eq(index).then($el => {
+//       tags_content.push($el.text());
+//     });
+//   }
 
-  tags_content.forEach(tag_content => {
-    const first_word = tag_content.split(" ");
-    let not_article = first_word != "A" || first_word != "An" || first_word != "The";
-    expect(not_article).to.be.equal(true);
-  });
-}
+//   tags_content.forEach(tag_content => {
+//     const first_word = tag_content.split(" ");
+//     let not_article = first_word != "A" || first_word != "An" || first_word != "The";
+//     expect(not_article).to.be.equal(true);
+//   });
+// }
